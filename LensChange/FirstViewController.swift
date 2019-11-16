accac9c60954ab98d7f33cca9cacd1300c9297a4
Dima Abramov <demween@gmail.com>
2019-11-16 10:59:51 +0300

    Initial Commit

---
+++ LensChange/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LensChange
+//
+//  Created by Дима on 16.11.2019.
+//  Copyright © 2019 Dima Abramov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LensChange/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LensChange
+//
+//  Created by Дима on 16.11.2019.
+//  Copyright © 2019 Dima Abramov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
