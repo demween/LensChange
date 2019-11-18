d994754cfc213ba34e1bcfaddafafcfb7a9d3930
Dima Abramov <demween@gmail.com>
2019-11-18 10:04:01 +0300

    Initial

--- LensChange/FirstViewController.swift
+++ LensChange/FirstViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var dayLeftTF: UITextField!
+    
+    
+    @IBAction func eyeButton() {
+        //MARK: Date Picker
+        let dayLeft = Date()
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: dayLeft)
+        print(day)
+    
+        
+        // MARK: Alert
+        let alert = UIAlertController(title: "", message: "Изменить время до замены?", preferredStyle: UIAlertController.Style.alert)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        alert.addAction(UIAlertAction(title: "Да", style: UIAlertAction.Style.default, handler: { action in
+            self.dayLeftTF.text = "30 Дней"
+            print("date was changed")
+        }))
+        alert.addAction(UIAlertAction(title: "Отмена", style: UIAlertAction.Style.cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+
+        
+        // MARK: Button pressed
+        print("button was pressed")
+//        dayLeftTF.text = "30 Дней"
     }
-
 
 }
 
